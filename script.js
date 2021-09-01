301dab92606c667caa0f995174c263f9b77403c3
Brittney Fernandez <bltorres1990@gmail.com>
2021-09-01 10:24:11 -0700

    start

---
+++ script.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    window.addEventListener('scroll', function() {
+        const nav = document.querySelector('.navbar');
+        if (this.scrollY > 20) {
+            nav.classList.add('sticky')
+        } else {
+            nav.classList.remove('sticky');
+        }
+      
+    });
+    document.querySelector('.menu-btn').addEventListener('click', function(){
+        document.querySelector('.navbar .menu').classList.toggle('active');
+
+    });
+})
